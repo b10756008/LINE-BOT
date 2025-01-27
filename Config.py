a2cda5ac8c68b0d24ed6f5992500189e07cbeb90
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-01-27 21:10:02 +0800

    Delete Config.py

--- Config.py
+++
@@ -1,8 +0,0 @@
-import os
-class Config():
-    def __init__(self):
-        self.accessToken = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-        self.channelSecret = os.getenv("LINE_CHANNEL_SECRET")
-        
-        self.accessToken2 = ""
-        self.channelSecret2 = ""
