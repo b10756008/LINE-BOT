648ce9e36d6efe211c1bbdae6c45bcb7c0c89bdb
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-02-01 15:56:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def handleTextMessage(event):
 
         replyM(configuration, rToken, flexM)
 
-    elif originalM == ("/認識硬體"):
+    if originalM == ("/認識硬體"):
         uS[uId] = UserStatus.SELECT_DEVICE
         imagemapMessage = ImagemapMessage(
             type="imagemap",
