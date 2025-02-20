68ea0d5425e3374edfc8787824f7f42493aae6a7
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-02-20 21:19:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -170,5 +170,5 @@ def handleTextMessage2(event):
 uS = {}
 uS2 = {}
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
