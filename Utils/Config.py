f4d6e18eb168534e10115b784e72bdeaf2ff3caa
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-02-20 23:04:33 +0800

    Add files via upload

--- Utils/Config.py
+++ Utils/Config.py
@@ -1,8 +1,10 @@
 import os
 class Config():
     def __init__(self):
-        self.accessToken = os.getenv("LINE_ACCESS_TOKEN", "")
-        self.channelSecret = os.getenv("LINE_CHANNEL_SECRET", "")
-
+        self.accessToken = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+        self.channelSecret = os.getenv("LINE_CHANNEL_SECRET")
+        # self.accessToken = ""
+        # self.channelSecret = ""
+    
         self.accessToken2 = ""
         self.channelSecret2 = ""
