dcad29caa5e46d8d5133219ea249575a89079774
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-02-20 21:51:54 +0800

    Update Config.py

--- Utils/Config.py
+++ Utils/Config.py
@@ -1,7 +1,7 @@
 class Config():
     def __init__(self):
-        self.accessToken = ""
-        self.channelSecret = ""
-        
+        self.accessToken = os.getenv("LINE_ACCESS_TOKEN", "")
+        self.channelSecret = os.getenv("LINE_CHANNEL_SECRET", "")
+
         self.accessToken2 = ""
         self.channelSecret2 = ""
